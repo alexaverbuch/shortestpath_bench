ad0269beb873aa0e87df67e9c9f9c995f8ebb9e6
Alex Averbuch <alex.averbuch@gmail.com>
2013-09-26 14:56:13 +0200

    added comparison of "unweighted dijkstra" (same result as shortest path), "weighted dijkstra", and "shortest path"

--- src/main/java/org/neo4j/bench/shortestpath/ShortestPathRunner.java
+++ src/main/java/org/neo4j/bench/shortestpath/ShortestPathRunner.java
@@ -1,6 +1,9 @@
 package org.neo4j.bench.shortestpath;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.HdrHistogram.Histogram;
@@ -9,13 +12,16 @@ import org.neo4j.graphalgo.CostEvaluator;
 import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
 import org.neo4j.graphalgo.WeightedPath;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Expander;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.PathExpander;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.kernel.Traversal;
 import org.neo4j.tooling.GlobalGraphOperations;
 
@@ -26,39 +32,93 @@ public class ShortestPathRunner
         GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder( Config.DB_DIR ).setConfig(
                 Config.NEO4J_CONFIG ).newGraphDatabase();
 
-        System.out.println( nodeCount( db, 1000 ) );
-        System.out.println( relationshipCount( db, 1000 ) );
+        System.out.println( "Node Count = " + nodeCount( db, 1000 ) );
+        System.out.println( "Relationship Count = " + relationshipCount( db, 1000 ) );
 
-        Histogram histogram = new Histogram( TimeUnit.MILLISECONDS.convert( 30, TimeUnit.SECONDS ), 5 );
-        histogram.recordValue( 100 );
-        histogram.recordValue( 1000 );
-        System.out.println( histogramStatsString( histogram ) );
+        Random random = new Random( 42 );
+        int runCount = 1000;
+        long minNodeId = 1;
+        long maxNodeId = 3900;
+        List<Pair<Node>> startAndEndNodes = generateStartAndEndNodes( random, db, runCount, minNodeId, maxNodeId );
 
         Expander expander = Traversal.expanderForAllTypes();
+
         CostEvaluator<Double> evaluator = CommonEvaluators.doubleCostEvaluator( "weight" );
-        PathFinder<WeightedPath> dijkstra = GraphAlgoFactory.dijkstra( expander, evaluator );
-        WeightedPath path = dijkstra.findSinglePath( db.getNodeById( 1 ), db.getNodeById( 101 ) );
-        System.out.println( path.length() );
+        int maxDepth = Integer.MAX_VALUE;
+        PathFinder<?> shortestPath = GraphAlgoFactory.shortestPath( expander, maxDepth );
+
+        CostEvaluator<Double> constantEvaluator = new CostEvaluator<Double>()
+        {
+            @Override
+            public Double getCost( Relationship relationship, Direction direction )
+            {
+                return 1D;
+            }
+        };
+        PathFinder<?> unweightedDijkstra = GraphAlgoFactory.dijkstra( expander, constantEvaluator );
+
+        PathFinder<?> weightedDijkstra = GraphAlgoFactory.dijkstra( expander, evaluator );
+
+        runPathFinder( shortestPath, startAndEndNodes );
+        runPathFinder( unweightedDijkstra, startAndEndNodes );
+        runPathFinder( weightedDijkstra, startAndEndNodes );
+
+        System.out.println( "- Shortest Path -\n" + runPathFinder( shortestPath, startAndEndNodes ) );
+        System.out.println( "- Unweighted Dijkstra -\n" + runPathFinder( unweightedDijkstra, startAndEndNodes ) );
+        System.out.println( "- Weighted Dijkstra -\n" + runPathFinder( weightedDijkstra, startAndEndNodes ) );
 
         db.shutdown();
-
-        // g = new Neo4jGraph( DATA_DIR );
-        // dijkstra = GraphAlgoFactory.dijkstra(
-        // Traversal.expanderForAllTypes(),
-        // CommonEvaluators.doubleCostEvaluator( "weight" ) );
-        // path = dijkstra.findSinglePath( g.v( START ).getRawVertex(), g.v( END
-        // ).getRawVertex() );
     }
 
-    public static String histogramStatsString( Histogram histogram )
+    public static String runPathFinder( PathFinder<?> pathFinder, List<Pair<Node>> startAndEndNodes )
+    {
+        Histogram timeHistogram = new Histogram( TimeUnit.MILLISECONDS.convert( 30, TimeUnit.SECONDS ), 5 );
+        Histogram pathLengthHistogram = new Histogram( 10000, 5 );
+        Path path = null;
+
+        for ( Pair<Node> startAndEndNode : startAndEndNodes )
+        {
+            long startTime = System.currentTimeMillis();
+            path = pathFinder.findSinglePath( startAndEndNode.getFirst(), startAndEndNode.getSecond() );
+            long runTime = System.currentTimeMillis() - startTime;
+            timeHistogram.recordValue( runTime );
+            pathLengthHistogram.recordValue( path.length() );
+        }
+        return timeHistogramString( timeHistogram ) + pathLengthHistogramString( pathLengthHistogram );
+    }
+
+    public static String timeHistogramString( Histogram histogram )
     {
         StringBuilder sb = new StringBuilder();
-        sb.append( "MIN\t\t" ).append( histogram.getHistogramData().getMinValue() ).append( "\n" );
-        sb.append( "MAX\t\t" ).append( histogram.getHistogramData().getMaxValue() ).append( "\n" );
-        sb.append( "50th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 50 ) ).append( "\n" );
-        sb.append( "90th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 90 ) ).append( "\n" );
-        sb.append( "95th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 95 ) ).append( "\n" );
-        sb.append( "99th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 99 ) ).append( "\n" );
+        sb.append( "\tRun Time\n" );
+        sb.append( "\t\tCOUNT\t\t" ).append( histogram.getHistogramData().getTotalCount() ).append( "\n" );
+        sb.append( "\t\tMIN\t\t" ).append( histogram.getHistogramData().getMinValue() ).append( "\n" );
+        sb.append( "\t\tMAX\t\t" ).append( histogram.getHistogramData().getMaxValue() ).append( "\n" );
+        sb.append( "\t\t50th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 50 ) ).append(
+                "\n" );
+        sb.append( "\t\t90th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 90 ) ).append(
+                "\n" );
+        sb.append( "\t\t95th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 95 ) ).append(
+                "\n" );
+        sb.append( "\t\t99th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 99 ) ).append(
+                "\n" );
+        return sb.toString();
+    }
+
+    public static String pathLengthHistogramString( Histogram histogram )
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append( "\tPath Length\n" );
+        sb.append( "\t\tMIN\t\t" ).append( histogram.getHistogramData().getMinValue() ).append( "\n" );
+        sb.append( "\t\tMAX\t\t" ).append( histogram.getHistogramData().getMaxValue() ).append( "\n" );
+        sb.append( "\t\t50th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 50 ) ).append(
+                "\n" );
+        sb.append( "\t\t90th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 90 ) ).append(
+                "\n" );
+        sb.append( "\t\t95th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 95 ) ).append(
+                "\n" );
+        sb.append( "\t\t99th percentile\t" ).append( histogram.getHistogramData().getValueAtPercentile( 99 ) ).append(
+                "\n" );
         return sb.toString();
     }
 
@@ -110,5 +170,66 @@ public class ShortestPathRunner
             throw new RuntimeException( e.getCause() );
         }
         return relationshipCount;
+    }
+
+    public static List<Pair<Node>> generateStartAndEndNodes( Random random, GraphDatabaseService db, int count,
+            long minId, long maxId )
+    {
+        List<Pair<Node>> startAndEndNodes = new ArrayList<Pair<Node>>();
+        long idRange = maxId - minId;
+        Transaction tx = db.beginTx();
+        try
+        {
+            for ( int i = 0; i < count; i++ )
+            {
+                long startNode = (long) ( ( random.nextDouble() * idRange ) + minId );
+                long endNode = -1;
+                while ( endNode == -1 )
+                {
+                    long val = (long) ( ( random.nextDouble() * idRange ) + minId );
+                    endNode = ( val == startNode ) ? -1 : val;
+                }
+                startAndEndNodes.add( new Pair( db.getNodeById( startNode ), db.getNodeById( endNode ) ) );
+            }
+            tx.success();
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e.getCause() );
+        }
+        finally
+        {
+            tx.finish();
+        }
+
+        return startAndEndNodes;
+    }
+
+    static class Pair<T>
+    {
+        private final T first;
+        private final T second;
+
+        private Pair( T first, T second )
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T getFirst()
+        {
+            return first;
+        }
+
+        public T getSecond()
+        {
+            return second;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Pair[" + first + "," + second + "]";
+        }
     }
 }
